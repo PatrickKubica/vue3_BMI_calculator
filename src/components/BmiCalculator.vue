0d4c54e1364e4ac88c77421eb955b0a2d76a5921
Patrick Kubica <mail@patrickkubica.de>
2021-01-23 13:22:57 +0100

    add check for empty weight or height

--- src/components/BmiCalculator.vue
+++ src/components/BmiCalculator.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <h1>BMI Calculator</h1>
-      <label for="height">body height in centimeters</label>
-      <input type="number" v-model="height" id="height">
-      <br/>
-      <label for="weight">body weight in kilogram</label>
-      <input type="number" v-model="weight" id="weight">
-      <p>BMI: {{ calculatedBmi }}</p>
+    <h1>BMI Calculator</h1>
+    <label for="height">body height in centimeters </label>
+    <input type="number" v-model="height" id="height" />
+    <br />
+    <label for="weight">body weight in kilogram </label>
+    <input type="number" v-model="weight" id="weight" />
+    <p>BMI: {{ calculatedBmi }}</p>
   </div>
 </template>
 
@@ -21,6 +21,9 @@ export default {
   },
   computed: {
     calculatedBmi() {
+      if (this.height === '' || this.weight === '') {
+        return '';
+      }
       const height = this.height / 100;
       return parseInt(this.weight / (height * height), 10);
     },
