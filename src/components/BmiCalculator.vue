c8d6b4e5e64f74f41fbd29bf6d9a1457e4b8a958
Patrick Kubica <mail@patrickkubica.de>
2021-01-24 14:34:44 +0100

    style frontend

--- src/components/BmiCalculator.vue
+++ src/components/BmiCalculator.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <h1>BMI Calculator</h1>
-    <label for="height">body height in centimeters </label>
-    <input type="number" v-model="height" id="height" />
-    <br />
-    <label for="weight">body weight in kilogram </label>
-    <input type="number" v-model="weight" id="weight" />
-    <p id="result">BMI: {{ calculatedBmi }}</p>
-    <div v-show="hasError" class="alert alert-danger" role="alert">
-      height and weight need to be positive numbers
+  <form>
+    <div class="form-group">
+      <h1 class="pb-5">BMI Calculator</h1>
+      <label for="height">body height in centimeters </label>
+      <input
+        type="number"
+        v-model="height"
+        id="height"
+        class="form-control text-center"
+      />
     </div>
-  </div>
+    <div class="form-group">
+      <label for="weight">body weight in kilogram </label>
+      <input
+        type="number"
+        v-model="weight"
+        id="weight"
+        class="form-control text-center"
+      />
+      <h2 id="result" class="pt-5">BMI: {{ calculatedBmi }}</h2>
+    </div>
+    <div class="form-group">
+      <div v-show="hasError" class="alert alert-danger" role="alert">
+        height and weight need to be positive numbers
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
